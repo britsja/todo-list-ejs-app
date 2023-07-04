33a0a696d7da20251850b5c2cf130ac45d6696db
Jan <pcerservices@gmail.com>
2023-07-04 16:40:40 +0200

    ejs added and list.ejs created which will contain to do list

--- app.js
+++ app.js
@@ -3,10 +3,22 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.listen(3000, function() {
-    console.log("Listening on port 3000");
+    console.log("Listening on port 3000!")
 })
 
 app.get("/", function(req, res) {
-    res.send("<h1>Hello</h1>")
+    var today = new Date();
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var d = new Date();
+    var dayName = days[d.getDay()];
+
+    if (today.getDay() === 6 || today.getDay() === 0) {
+        var weekend = "a weekend baby!"
+        res.render('list', {dayName: weekend});
+    } else {
+        res.render('list', {dayName: dayName});
+    }
 })
