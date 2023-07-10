4c1c2a499cabeb5de75bbf1fe4e1cacb57849aea
Jan <pcerservices@gmail.com>
2023-07-10 13:01:32 +0200

    Added functionality to do post requests and add the html list

--- app.js
+++ app.js
@@ -3,22 +3,34 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set('view engine', 'ejs');
 
 app.listen(3000, function() {
     console.log("Listening on port 3000!")
 })
 
+var itemList = [];
+
 app.get("/", function(req, res) {
     var today = new Date();
+    
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var d = new Date();
     var dayName = days[d.getDay()];
 
     if (today.getDay() === 6 || today.getDay() === 0) {
         var weekend = "a weekend baby!"
-        res.render('list', {dayName: weekend});
+        res.render('list', {dayName: weekend, itemList: itemList});
     } else {
-        res.render('list', {dayName: dayName});
+        res.render('list', {dayName: dayName, itemList: itemList});
     }
+})
+
+app.post("/", function(req, res) {
+    const newItem = req.body.newItem;    
+    itemList.push(newItem);    
+    res.redirect("/");
+    
 })
