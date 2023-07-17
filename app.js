14ba7b3441b8198cd489e5ec630492bd9d765976
Jan <pcerservices@gmail.com>
2023-07-17 17:48:45 +0200

    Added mongoose and cors and connected to Atlas DB

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const date = require(__dirname + "/date.js");
-
+require('dotenv').config({path: __dirname + '/.env'});
 const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -10,8 +12,20 @@ app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
+app.use(cors());
+
+const uri = process.env.ATLAS_URI;
+
+console.log(uri);
+
 app.listen(3000, function() {
     console.log("Listening on port 3000!")
+})
+
+mongoose.connect(uri);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
 })
 
 var itemList = [];
